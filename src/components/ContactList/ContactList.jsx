79adebaa590a895d031bd05ac0d9ef875286ee5c
NEYROSICK <dhatak24@gmail.com>
2023-08-19 17:04:44 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,6 +2,7 @@ import ContactItem from 'components/ContactItem';
 import PropTypes from 'prop-types';
 import cl from 'components/ContactList/contactList.module.css';
 import React from 'react';
+
 class ContactList extends React.Component {
   componentDidMount = () => {
     const newContacts = JSON.parse(localStorage.getItem('contacts'));
@@ -16,35 +17,41 @@ class ContactList extends React.Component {
       updateContacts(newContacts);
     };
 
-    if (!state.contacts.length) {
-      return (
-        <p className={cl.emptyMessage}>
-          Complete Emptiness {':('}
-          <br /> Try to add some contacts to your phonebook
-        </p>
-      );
-    } else if (!filteredContacts.length && state.filter) {
-      return (
-        <p className={cl.emptyMessage}>
-          Sorry, there is no such contact in your phonebook
-        </p>
-      );
+    if (state.contacts) {
+      if (!state.contacts.length) {
+        return (
+          <p className={cl.emptyMessage}>
+            Complete Emptiness {':('}
+            <br /> Try to add some contacts to your phonebook
+          </p>
+        );
+      } else if (!filteredContacts.length && state.filter) {
+        return (
+          <p className={cl.emptyMessage}>
+            Sorry, there is no such contact in your phonebook
+          </p>
+        );
+      } else {
+        return (
+          <ul className={cl.list}>
+            {filteredContacts.map(contact => {
+              return (
+                <ContactItem
+                  deleteContact={deleteContact}
+                  key={contact.id}
+                  name={contact.name}
+                  number={contact.number}
+                  url={
+                    'https://cdn-icons-png.flaticon.com/128/1177/1177568.png'
+                  }
+                />
+              );
+            })}
+          </ul>
+        );
+      }
     } else {
-      return (
-        <ul className={cl.list}>
-          {filteredContacts.map(contact => {
-            return (
-              <ContactItem
-                deleteContact={deleteContact}
-                key={contact.id}
-                name={contact.name}
-                number={contact.number}
-                url={'https://cdn-icons-png.flaticon.com/128/1177/1177568.png'}
-              />
-            );
-          })}
-        </ul>
-      );
+      return null;
     }
   };
 
